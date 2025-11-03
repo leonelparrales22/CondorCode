c19fb17c364ffbae7af6a38d5a985d7ae2964ead
Leonel Parrales M <parralesdl@produbanco.com>
2025-11-02 22:24:40 -0500

    Refactor website structure: enhance styles, update loading screen, and improve responsiveness

--- script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', function() {
-    setTimeout(function() {
-        document.getElementById('loading').style.display = 'none';
-    }, 2000); // Hide after 2 seconds for animation
-});
